025fac97696943b4d18f27b194eb8d04f1eab48c
Kaio Kaio <kaiofelipee12@gmail.com>
2023-07-25 10:33:39 -0300

    Login com banco de dados

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+    $dbHost = 'LocalHost';
+    $dbUsername = 'root';
+    $dbPassword = 'Kaioeclara17!';
+    $dbName = 'formulario-kaio';
+
+   $conexao = new mysqli ($dbHost,$dbUsername,$dbPassword,$dbName);
+
+    //if($conexao ->connect_errno){
+    //    echo "erro";
+   // }
+   // else{
+   //     echo "certo";
+   // }
+
+
+
+
+
+?>
